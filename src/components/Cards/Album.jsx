cde215387340c2676073931fa43b47a80aa6e4f3
Rafael <rafaelmartinez.contato@gmail.com>
2022-02-21 13:53:26 -0300

    ajustando css no card de album

--- src/components/Cards/Album.jsx
+++ src/components/Cards/Album.jsx
@@ -2,15 +2,14 @@
 import { ArtistTitle } from "./ArtistTitle";
 
 export const Album = ({ artists, name, id, image }) => {
-  
   return (
     <div className="album">
       <img
-        src={image}         //ADICIONAR O LINK QUE VIAJA PARA PÁGINA DO ALBUM EM QUESTÃO USANDO O ID PASSADO
-        alt = {`${name} album`}
+        src={image} //ADICIONAR O LINK QUE VIAJA PARA PÁGINA DO ALBUM EM QUESTÃO USANDO O ID PASSADO
+        alt={`${name} album`}
       />
       <h3>{name}</h3>
-      <ArtistTitle artists={artists}/>
+      <ArtistTitle artists={artists} />
     </div>
   );
 };
